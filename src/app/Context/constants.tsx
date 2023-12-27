2ec3039fb06a71843054e9972448999f1dc3dc08
ajmalfjordstans <frontend.fjordstans@gmail.com>
2023-12-27 19:13:39 +0530

    update

--- src/app/Context/constants.tsx
+++ src/app/Context/constants.tsx
@@ -1,67 +1,75 @@
 export const CONST_PRODUCTS = [
   {
-    img: `/image/ev/Products/product1.png`,
-    category: "Home GO",
-    title: "POWERPOD Home",
-    desc: "7.4kW AC Fast charger",
-    price: "₹45,000.00",
-    offPrice: "$79",
-  },
-  {
-    img: `/image/ev/Products/product2.png`,
-    category: "POWERPOD Home",
-    title: "EV Charging station",
-    desc: "POWERPOD Home for Personal use",
-    price: "₹15,000.00",
-    offPrice: "$79",
-  },
-  {
-    img: `/image/ev/Products/product3.png`,
-    category: "Dual",
-    title: "POWERPOD CITY",
-    desc: "22Kw Dual Port Fast EV Charger",
-    price: "₹140,000.00",
-    offPrice: "$79",
-  },
-  {
-    img: `/image/ev/Products/product5.png`,
+    id: 1,
+    img: "/image/ev/Products/home.png",
     category: "Home GO",
     title: "PowerPod Home",
-    desc: "22Kw Fast EV Charger",
-    price: "₹55,000.00",
-    offPrice: "$79",
+    desc: "3.3kW POWERPOD Home for Personal use",
+    price: "Rs. 15,000.00"
   },
   {
-    img: `/image/ev/Products/product5.png`,
-    category: "Commercial",
-    title: "PowerPod Commercial",
-    desc: "7.4 kW Fast EV Charger",
-    price: "₹45,000.00",
-    offPrice: "$79",
+    id: 2,
+    img: "/image/ev/Products/home.png",
+    category: "Home GO",
+    title: "PowerPod Home",
+    desc: "7.4kW AC Fast charger",
+    price: "Rs. 45,000.00"
   },
   {
-    img: `/image/ev/Products/product4.png`,
+    id: 3,
+    img: "/image/ev/Products/home.png",
+    category: "Home GO",
+    title: "PowerPod Home",
+    desc: "22kW Fast EV Charger",
+    price: "Rs. 55,000.00"
+  },
+  {
+    id: 4,
+    img: "/image/ev/Products/city.png",
+    category: "Dual",
+    title: "PowerPod City",
+    desc: "3.3kW ",
+    price: "Rs. 55,000.00"
+  },
+  {
+    id: 5,
+    img: "/image/ev/Products/city.png",
+    category: "Dual",
+    title: "PowerPod City",
+    desc: "7.4kW Hybrid EV Charging station Dual Port",
+    price: "Rs. 110,000.00"
+  },
+  {
+    id: 6,
+    img: "/image/ev/Products/city.png",
+    category: "Dual",
+    title: "PowerPod City",
+    desc: "22kW Dual Port Fast EV Charger",
+    price: "Rs. 140,000.00"
+  },
+  {
+    id: 7,
+    img: "/image/ev/Products/commercial3.3.png",
     category: "Commercial",
     title: "PowerPod Commercial",
     desc: "3.3kW Commercial EV Charger",
-    price: "₹18,000.00",
-    offPrice: "$79",
+    price: "Rs. 18,000.00"
   },
   {
-    img: `/image/ev/Products/product3.png`,
-    category: "Dual",
-    title: "POWERPOD City",
-    desc: "Hybrid EV Charging station Dual Port 7.4Kw",
-    price: "₹110,000.00",
-    offPrice: "$79",
-  },
-  {
-    img: `/image/ev/Products/product5.png`,
+    id: 8,
+    img: "/image/ev/Products/commercial.png",
     category: "Commercial",
     title: "PowerPod Commercial",
-    desc: "22 kW Fast EV Charger",
-    price: "₹55,000.00",
-    offPrice: "$79",
+    desc: "7.4kW Fast EV Charger",
+    price: "Rs. 58,000.00"
+  },
+  {
+    id: 9,
+    img: "/image/ev/Products/commercial.png",
+    category: "Commercial",
+    title: "PowerPod Commercial",
+    desc: "22kW kW Fast EV Charger",
+    price: "Rs. 70,000.00"
   },
 ];
 
@@ -71,33 +79,51 @@ export const INDUSTRY_CATEGORIES = {
     desc: "Introducing the PowerPod City series, your gateway to seamless electric vehicle (EV) charging in urban spaces. Tailored for parking areas, these charging stations combine accessibility, efficiency, and modern design to meet the unique needs of city dwellers.",
     products: [
       {
-        title: "PowerPod City 3.3kW",
+        id: 4,
         img: "/image/ev/Products/city.png",
+        category: "Dual",
+        title: "PowerPod City",
+        desc: "3.3kW ",
         price: "Rs. 55,000.00"
       },
       {
-        title: "PowerPod City 7.4kW",
+        id: 5,
         img: "/image/ev/Products/city.png",
+        category: "Dual",
+        title: "PowerPod City",
+        desc: "7.4kW Hybrid EV Charging station Dual Port",
         price: "Rs. 110,000.00"
       },
       {
-        title: "PowerPod City 22kW",
+        id: 6,
         img: "/image/ev/Products/city.png",
+        category: "Dual",
+        title: "PowerPod City",
+        desc: "22kW Dual Port Fast EV Charger",
         price: "Rs. 140,000.00"
       },
       {
-        title: "PowerPod Commercial 3.3kW",
-        img: "/image/ev/Products/commercial.png",
+        id: 7,
+        img: "/image/ev/Products/commercial3.3.png",
+        category: "Commercial",
+        title: "PowerPod Commercial",
+        desc: "3.3kW Commercial EV Charger",
         price: "Rs. 18,000.00"
       },
       {
-        title: "PowerPod Commercial 7.4kW",
+        id: 8,
         img: "/image/ev/Products/commercial.png",
+        category: "Commercial",
+        title: "PowerPod Commercial",
+        desc: "7.4kW Fast EV Charger",
         price: "Rs. 58,000.00"
       },
       {
-        title: "PowerPod Commercial 22kW",
+        id: 9,
         img: "/image/ev/Products/commercial.png",
+        category: "Commercial",
+        title: "PowerPod Commercial",
+        desc: "22kW kW Fast EV Charger",
         price: "Rs. 70,000.00"
       },
     ]
@@ -107,18 +133,27 @@ export const INDUSTRY_CATEGORIES = {
     desc: "Transform your residence into an electric vehicle (EV) haven with the PowerPod Home series. Designed for convenience and efficiency, our home charging stations provide a reliable solution for EV owners, ensuring your vehicle is always ready for the next journey.",
     products: [
       {
-        title: "PowerPod Home 3.3kW",
+        id: 1,
         img: "/image/ev/Products/home.png",
+        category: "Home GO",
+        title: "PowerPod Home",
+        desc: "3.3kW POWERPOD Home for Personal use",
         price: "Rs. 15,000.00"
       },
       {
-        title: "PowerPod Home 7.4kW",
+        id: 2,
         img: "/image/ev/Products/home.png",
+        category: "Home GO",
+        title: "PowerPod Home",
+        desc: "7.4kW AC Fast charger",
         price: "Rs. 45,000.00"
       },
       {
-        title: "PowerPod Home 22kW",
+        id: 3,
         img: "/image/ev/Products/home.png",
+        category: "Home GO",
+        title: "PowerPod Home",
+        desc: "22kW Fast EV Charger",
         price: "Rs. 55,000.00"
       },
     ]
@@ -128,18 +163,27 @@ export const INDUSTRY_CATEGORIES = {
     desc: "Elevate your charging infrastructure with the PowerPod Commercial series, designed to meet the evolving needs of electric vehicle (EV) charging in workplace environments. Our commercial charging stations combine cutting-edge technology with robust performance, ensuring a seamless charging experience for your employees and visitors.",
     products: [
       {
-        title: "PowerPod Commercial 3.3kW",
-        img: "/image/ev/Products/commercial.png",
+        id: 7,
+        img: "/image/ev/Products/commercial3.3.png",
+        category: "Commercial",
+        title: "PowerPod Commercial",
+        desc: "3.3kW Commercial EV Charger",
         price: "Rs. 18,000.00"
       },
       {
-        title: "PowerPod Commercial 7.4kW",
+        id: 8,
         img: "/image/ev/Products/commercial.png",
+        category: "Commercial",
+        title: "PowerPod Commercial",
+        desc: "7.4kW Fast EV Charger",
         price: "Rs. 58,000.00"
       },
       {
-        title: "PowerPod Commercial 22kW",
+        id: 9,
         img: "/image/ev/Products/commercial.png",
+        category: "Commercial",
+        title: "PowerPod Commercial",
+        desc: "22kW kW Fast EV Charger",
         price: "Rs. 70,000.00"
       },
     ]
