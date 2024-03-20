946510c5f77e885402d7e7623e99c97dac175fa0
mohammedajmalgit <mdajmalmt@gmail.com>
2024-03-20 17:09:29 +0530

    facebook pixel code added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,11 +30,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Facebook Pixel Code */}
         <script
-          defer
-          data-site="YOUR_DOMAIN_HERE"
-          src="https://api.nepcha.com/js/nepcha-analytics.js"
-        ></script>
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '872008484611145');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=872008484611145&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Facebook Pixel Code */}
         <meta property="og:title" content="EV Life" />
         <meta property="og:description" content="Explore our range of EV chargers for home and commercial use. High-quality charging solutions for electric vehicles." />
         <meta property="og:image" content="/logos/logo-ev-life.png" />
